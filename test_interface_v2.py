f5bed0ea8279123ef7b10720debb6380b674d8fe
lea-danober <lea.danober@gmail.com>
2026-02-26 14:52:10 +0100

    final

--- test_interface_v2.py
+++ test_interface_v2.py
@@ -7,7 +7,7 @@ from tkinter import Canvas
 class Interface(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.geometry(f"{800}x{650}")
+        self.geometry(f"{800}x{500}")
         self.title("Effect pedal")   
 
         self.t0 = ttk.Label(self, text = "General", font="Serif 10 bold", padding = 20)
@@ -26,37 +26,6 @@ class Interface(tk.Tk):
 
         self.val0 = ttk.Label(self.row0, text = "0.00", font="Serif 10 bold", padding = 15)
         self.val0.pack(side = tk.RIGHT)
-
-        self.t1 = ttk.Label(self, text = "Echo", font="Serif 10 bold", padding = 20)
-        self.t1.pack(side = tk.TOP)
-
-        self.row11 = ttk.Frame(self)
-        self.row11.pack(fill="x", padx=[20, 50])
-
-        self.st11 = ttk.Label(self.row11, text = "Duration", font="Serif 10 bold", padding = 15)
-        self.st11.pack(side = tk.LEFT)
-
-        self.v11 = tk.DoubleVar()
-        self.s11 = ttk.Scale(self.row11, from_ = 0, to = 0.25, length = 525, orient = tk.HORIZONTAL, variable = self.v11)
-        self.s11.bind("<ButtonRelease-1>", self.send_values)
-        self.s11.pack(side = tk.RIGHT)
-
-        self.val11 = ttk.Label(self.row11, text = "0.00", font="Serif 10 bold", padding = 15)
-        self.val11.pack(side = tk.RIGHT)
-
-        self.row12 = ttk.Frame(self)
-        self.row12.pack(fill="x", padx=[20, 50])
-
-        self.st12 = ttk.Label(self.row12, text = "Feedback coeff", font="Serif 10 bold", padding = 15)
-        self.st12.pack(side = tk.LEFT)
-
-        self.v12 = tk.DoubleVar()
-        self.s12 = ttk.Scale(self.row12, from_ = 0, to = 0.95, length = 525, orient = tk.HORIZONTAL, variable = self.v12)
-        self.s12.bind("<ButtonRelease-1>", self.send_values)
-        self.s12.pack(side = tk.RIGHT)
-
-        self.val12 = ttk.Label(self.row12, text = "0.00", font="Serif 10 bold", padding = 15)
-        self.val12.pack(side = tk.RIGHT)
 
         self.t2 = ttk.Label(self, text = "Distorsion", font="Serif 10 bold", padding = 20)
         self.t2.pack(side = tk.TOP)
@@ -128,22 +97,18 @@ class Interface(tk.Tk):
 
     def send_values(self, value):
         self.val0.config(text=f"{float(self.v0.get()):.2f}")
-        self.val11.config(text=f"{float(self.v11.get()):.2f}")
-        self.val12.config(text=f"{float(self.v12.get()):.2f}")
         self.val21.config(text=f"{float(self.v21.get()):.1f}")
         self.val22.config(text=f"{float(self.v22.get()):.0f}")
         self.val31.config(text=f"{float(self.v31.get()):.2f}")
         self.val32.config(text=f"{float(self.v32.get()):.1f}")
 
         volume = self.v0.get()
-        duration = self.v11.get()
-        feedback = self.v12.get()
         drive = self.v21.get()
         tone = self.v22.get()
         rate = self.v31.get()
         depth = self.v32.get()
 
-        message = f"{volume},{duration},{feedback},{drive},{tone},{rate},{depth}\n"
+        message = f"{volume},{drive},{tone},{rate},{depth}\n"
 
         if self.ser.is_open:
             self.ser.write(message.encode())
